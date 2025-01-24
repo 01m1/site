78bfc9cfdbf7249621e6eab535dff36436267625
01m1 <69215780+01m1@users.noreply.github.com>
2025-01-24 17:29:12 +0000

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div></div>
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-500">Osi Mayungbo</h1>
+      </div>
     </>
   );
 }
